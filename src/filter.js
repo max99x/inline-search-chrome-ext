58eec948a590c85874c7f33be87b1c26d874c560
max99x <max99x@5a079dfc-2337-11df-99a1-e5276f3be39f>
2011-05-20 18:49:03 +0000

    Minor space/style fixes. No change in functionality.

--- src/filter.js
+++ src/filter.js
@@ -1,29 +1,29 @@
 // Find an element's absolute position, taken from:
 // http://www.quirksmode.org/js/findpos.html
 function findPos(obj) {
-	var curleft = curtop = 0;
-	
+  var curleft = 0, curtop = 0;
+
   do {
-      curleft += obj.offsetLeft;
-      curtop += obj.offsetTop;
+    curleft += obj.offsetLeft;
+    curtop += obj.offsetTop;
   } while (obj = obj.offsetParent);
-	
-	return [curleft, curtop];
+
+  return [curleft, curtop];
 }
 
 function getFirstElementOfClass(cls) {
   var tags = document.getElementsByTagName('*');
-  
-  for (var i in tags) { 
-    if (tags[i].className == cls) { 
+
+  for (var i in tags) {
+    if (tags[i].className == cls) {
       return tags[i]
-    } 
+    }
   }
-  
+
   return null;
 }
 
-function applyScrolling(target, x, y) {  
+function applyScrolling(target, x, y) {
   if (target) {
     anchor = document.getElementById(target) || getFirstElementOfClass(target);
     if (anchor) {
@@ -33,16 +33,16 @@ function applyScrolling(target, x, y) {
       setTimeout(function() {applyScrolling(target, x, y);}, 200);
     }
   }
-  
+
   if (x || y) {
     scrollBy(x, y);
   }
 }
 
-
 // Main procedure.
 if (window != top) {
-  chrome.extension.sendRequest({method: 'get_filter', arg: String(window.location)}, function(response) {
+  var message = {method: 'get_filter', arg: String(window.location)};
+  chrome.extension.sendRequest(message, function(response) {
     if (response) {
       applyScrolling(response.target, response.scroll.x, response.scroll.y);
     }
